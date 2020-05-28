62ce27f114290f47e83a6f1e7de167c8461f92b2
Sebyby <sebyby@users.noreply.github.com>
2020-05-28 08:35:15 +0200

    added viz 5+6

---
+++ website/js/dv/race_actor.js
@@ -0,0 +1,271 @@
+let selectGenre = "Comedy"
+
+$(function () {
+    $("#actor_dropdown li a").click(function () {
+        $(".btn").text($(this).text());
+        $(".btn").val($(this).text());
+        selectGenre = $(this).text();
+
+        choose_actor_race();
+    });
+});
+
+$(function () { choose_actor_race(); });
+
+/*let title = svgRaceActors.append('text')
+ .attr('class', 'title')
+ .attr('y', 24)
+ .html('18 years of Interbrand’s Top Global Brands');*/
+
+
+let actor_race_ticker;
+let draw_actor_race;
+
+choose_actor_race = function () {
+    
+    let viz = document.getElementById("actors_race_viz");
+    if(viz.hasChildNodes()){
+        viz.removeChild(viz.firstChild);
+    }
+
+    var tickDuration = 500;
+
+    var svgRaceActors = d35.select("#actors_race_viz").append("svg")
+    .attr("width", 960)
+    .attr("height", 600);
+
+    //d35.csv('brand_values.csv').then(function(data) {
+    d35.csv('./data/career_race_' + selectGenre + '.csv').then(function (data) {
+        //if (error) throw error;
+        console.log("in")
+        var top_n = 6;
+        let year = 1910;
+        height = 600
+
+        let subTitle = svgRaceActors.append("text")
+            .attr("class", "subTitle")
+            .attr("y", 25)
+            .html("Number of movies in last 10 years");
+
+        let caption = svgRaceActors.append('text')
+            .attr('class', 'caption')
+            .attr('x', width)
+            .attr('y', height - 5)
+            .style('text-anchor', 'end')
+            .html('Source: Interbrand');
+
+        const marginRace = {
+            top: 50,
+            right: 0,
+            bottom: 5,
+            left: 0
+        };
+        let barPadding = (height - (marginRace.bottom + marginRace.top)) / (top_n * 5);
+        data.forEach(d => {
+            d.value = +d.value,
+                d.lastValue = +d.lastValue,
+                d.value = isNaN(d.value) ? 0 : d.value,
+                d.year = +d.year,
+                d.colour = d35.hsl(Math.random() * 360, 0.75, 0.75)
+        });
+
+
+        let yearSlice = data.filter(d => d.year == year && !isNaN(d.value))
+            .sort((a, b) => b.value - a.value)
+            .slice(0, top_n);
+
+        yearSlice.forEach((d, i) => d.rank = i);
+
+
+        let x = d35.scaleLinear()
+            .domain([0, d35.max(yearSlice, d => d.value)])
+            .range([marginRace.left, width - marginRace.right - 65]);
+
+        let y = d35.scaleLinear()
+            .domain([top_n, 0])
+            .range([height - marginRace.bottom, marginRace.top]);
+
+        let xAxis = d35.axisTop()
+            .scale(x)
+            .ticks(width > 500 ? 5 : 2)
+            .tickSize(-(height - marginRace.top - marginRace.bottom))
+            .tickFormat(d => d35.format(',')(d));
+
+        svgRaceActors.append('g')
+            .attr('class', 'axis xAxis')
+            .attr('transform', `translate(0, ${marginRace.top})`)
+            .call(xAxis)
+            .selectAll('.tick line')
+            .classed('origin', d => d == 0);
+
+        svgRaceActors.selectAll('rect.bar')
+            .data(yearSlice, d => d.name)
+            .enter()
+            .append('rect')
+            .attr('class', 'bar')
+            .attr('x', x(0) + 1)
+            .attr('width', d => x(d.value) - x(0) - 1)
+            .attr('y', d => y(d.rank) + 5)
+            .attr('height', y(1) - y(0) - barPadding)
+            .style('fill', d => d.colour);
+
+        svgRaceActors.selectAll('text.label')
+            .data(yearSlice, d => d.name)
+            .enter()
+            .append('text')
+            .attr('class', 'label')
+            .attr('x', d => x(d.value) - 8)
+            .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1)
+            .style('text-anchor', 'end')
+            .html(d => d.name);
+
+        svgRaceActors.selectAll('text.valueLabel')
+            .data(yearSlice, d => d.name)
+            .enter()
+            .append('text')
+            .attr('class', 'valueLabel')
+            .attr('x', d => x(d.value) + 5)
+            .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1)
+            .text(d => d35.format(',.0f')(d.lastValue));
+
+        let yearText = svgRaceActors.append('text')
+            .attr('class', 'yearText')
+            .attr('x', width - marginRace.right)
+            .attr('y', height - 25)
+            .style('text-anchor', 'end')
+            .html(~~year)
+            .call(halo, 10);
+
+        draw_actor_race = function () {
+
+            yearSlice = data.filter(d => d.year == year && !isNaN(d.value))
+                .sort((a, b) => b.value - a.value)
+                .slice(0, top_n);
+
+            yearSlice.forEach((d, i) => d.rank = i);
+
+            //console.log('IntervalYear: ', yearSlice);
+
+            x.domain([0, d35.max(yearSlice, d => d.value)]);
+
+            svgRaceActors.select('.xAxis')
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .call(xAxis);
+
+            let bars = svgRaceActors.selectAll('.bar').data(yearSlice, d => d.name);
+
+            bars
+                .enter()
+                .append('rect')
+                .attr('class', d => `bar ${d.name.replace(/\s/g, '_')}`)
+                .attr('x', x(0) + 1)
+                .attr('width', d => x(d.value) - x(0) - 1)
+                .attr('y', d => y(top_n + 1) + 5)
+                .attr('height', y(1) - y(0) - barPadding)
+                .style('fill', d => d.colour)
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('y', d => y(d.rank) + 5);
+
+            bars
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('width', d => x(d.value) - x(0) - 1)
+                .attr('y', d => y(d.rank) + 5);
+
+            bars
+                .exit()
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('width', d => x(d.value) - x(0) - 1)
+                .attr('y', d => y(top_n + 1) + 5)
+                .remove();
+
+            let labels = svgRaceActors.selectAll('.label')
+                .data(yearSlice, d => d.name);
+
+            labels
+                .enter()
+                .append('text')
+                .attr('class', 'label')
+                .attr('x', d => x(d.value) - 8)
+                .attr('y', d => y(top_n + 1) + 5 + ((y(1) - y(0)) / 2))
+                .style('text-anchor', 'end')
+                .html(d => d.name)
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1);
+
+
+            labels
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('x', d => x(d.value) - 8)
+                .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1);
+
+            labels
+                .exit()
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('x', d => x(d.value) - 8)
+                .attr('y', d => y(top_n + 1) + 5)
+                .remove();
+
+
+
+            let valueLabels = svgRaceActors.selectAll('.valueLabel').data(yearSlice, d => d.name);
+
+            valueLabels
+                .enter()
+                .append('text')
+                .attr('class', 'valueLabel')
+                .attr('x', d => x(d.value) + 5)
+                .attr('y', d => y(top_n + 1) + 5)
+                .text(d => d35.format(',.0f')(d.lastValue))
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1);
+
+            valueLabels
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('x', d => x(d.value) + 5)
+                .attr('y', d => y(d.rank) + 5 + ((y(1) - y(0)) / 2) + 1)
+                .tween("text", function (d) {
+                    let i = d35.interpolateRound(d.lastValue, d.value);
+                    return function (t) {
+                        this.textContent = d35.format(',')(i(t));
+                    };
+                });
+
+
+            valueLabels
+                .exit()
+                .transition()
+                .duration(tickDuration)
+                .ease(d35.easeLinear)
+                .attr('x', d => x(d.value) + 5)
+                .attr('y', d => y(top_n + 1) + 5)
+                .remove();
+
+            yearText.html(~~year);
+
+            if (year >= 2018) actor_race_ticker.stop();
+            year += 1
+            //year = d35.format('.1f')((+year) + 0.1);
+        };
+
+/*let*/ actor_race_ticker = d35.interval(e => draw_actor_race(), tickDuration);
+
+    });
+}
---
+++ website/js/dv/span.js
@@ -0,0 +1,198 @@
+
+let sorting = 'time'
+
+$(function () {
+  $("#span_dropdown li a").click(function () {
+    $(".btn").text($(this).text());
+    $(".btn").val($(this).text());
+    sorting = $(this).text();
+    draw_span();
+  });
+});
+
+$(function () { draw_span(); });
+
+formatDate = function (d) {
+  return d;
+};
+
+getTooltipContent = function (d) {
+  return `<b>${d.actor}</b>
+  <br/>
+  <b style="color:${d.color.darker()}">${d.genre}</b>
+  <br/>
+  ${formatDate(parseInt(d.start))} - ${formatDate(parseInt(d.end))}
+  `
+};
+
+createTooltip = function (el) {
+  el
+    .style("position", "absolute")
+    .style("pointer-events", "none")
+    .style("top", 0)
+    .style("opacity", 0)
+    .style("background", "white")
+    .style("border-radius", "5px")
+    .style("box-shadow", "0 0 10px rgba(0,0,0,.25)")
+    .style("padding", "10px")
+    .style("line-height", "1.3")
+    .style("font", "11px sans-serif")
+};
+
+getRect = function (d) {
+  const el = d35.select(this);
+  const sx = x(d.start);
+  const w = x(d.end) - x(d.start);
+  const isLabelRight = (sx > width / 2 ? sx + w < width : sx - w > 0);
+
+  el.style("cursor", "pointer")
+
+  el
+    .append("rect")
+    .attr("x", sx)
+    .attr("height", y.bandwidth())
+    .attr("width", w)
+    .attr("fill", d.color);
+
+  el
+    .append("text")
+    .text(d.actor)
+    .attr("x", isLabelRight ? sx - 5 : sx + w + 5)
+    .attr("y", 2.5)
+    .attr("fill", "black")
+    .style("text-anchor", isLabelRight ? "end" : "start")
+    .style("dominant-baseline", "hanging")
+    .style("font", "11px sans-serif");
+};
+
+draw_span = function () {
+
+  csv = d35.csv("./data/span.csv").then(function (data) {
+
+    height = 1000
+    data.map(d => {
+      return {
+        ...d,
+        start: +d.start,
+        end: +d.end
+      }
+    }).sort((a, b) => parseInt(a.start) - parseInt(b.start));
+
+    const margin = ({ top: 50, right: 30, bottom: 30, left: 30 });
+    y = d35.scaleBand()
+      .domain(d35.range(data.length))
+      .range([0, height - margin.bottom - margin.top])
+      .padding(0.2)
+
+    x = d35.scaleLinear()
+      .domain([d35.min(data, d => parseInt(d.start)), d35.max(data, d => parseInt(d.end))])
+      .range([0, width - margin.left - margin.right])
+
+
+    let genres = d35.nest().key(d => d.genre).entries(data).map(d => d.key)
+
+    let dataByGenre = d35.nest().key(d => d.genre).entries(data);
+    dataByGenre.forEach(s => s.values.sort((a, b) => parseInt(a.start) - parseInt(b.start)));
+    console.log(dataByGenre);
+
+    let axisBottom = d35.axisBottom(x)
+      .tickPadding(2)
+      .tickFormat(formatDate)
+
+    let axisTop = d35.axisTop(x)
+      .tickPadding(2)
+      .tickFormat(formatDate)
+
+    color = d35.scaleOrdinal(d35.schemeSet2).domain(genres)
+
+    let filteredData;
+
+    if (sorting !== "Time") {
+      filteredData = [].concat.apply([], dataByGenre.map(d => d.values));
+    } else {
+      filteredData = data.sort((a, b) => parseInt(a.start) - parseInt(b.start));
+    }
+
+    filteredData.forEach(d => d.color = d35.color(color(d.genre)))
+
+    let parent = document.getElementById('span_viz');
+
+    if (!parent.hasChildNodes()) {
+      var svg = d35.select("#span_viz").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+      const g = svg.append("g").attr("transform", (d, i) => `translate(${margin.left} ${margin.top})`);
+
+      const groups = g
+        .selectAll("g")
+        .data(filteredData)
+        .enter()
+        .append("g")
+        .attr("class", "civ")
+
+
+      const tooltip = d35.select(document.createElement("div")).call(createTooltip);
+
+      const line = svg.append("line").attr("y1", margin.top - 10).attr("y2", height - margin.bottom).attr("stroke", "rgba(0,0,0,0.2)").style("pointer-events", "none");
+
+      groups.attr("transform", (d, i) => `translate(0 ${y(i)})`)
+
+      groups
+        .each(getRect)
+        .on("mouseover", function (d) {
+          d35.select(this).select("rect").attr("fill", d.color.darker())
+
+          tooltip
+            .style("opacity", 1)
+            .html(getTooltipContent(d))
+        })
+        .on("mouseleave", function (d) {
+          d35.select(this).select("rect").attr("fill", d.color)
+          tooltip.style("opacity", 0)
+        })
+
+
+      svg
+        .append("g")
+        .attr("transform", (d, i) => `translate(${margin.left} ${margin.top - 10})`)
+        .call(axisTop)
+
+      svg
+        .append("g")
+        .attr("transform", (d, i) => `translate(${margin.left} ${height - margin.bottom})`)
+        .call(axisBottom)
+
+
+
+      svg.on("mousemove", function (d) {
+
+        let [x, _] = d35.mouse(this);
+        y = d35.event.pageY;
+        line.attr("transform", `translate(${x} 0)`);
+        y -= 120;
+        if (x > width / 2) x -= 100;
+        tooltip
+          .style("left", x + "px")
+          .style("top", y + "px")
+      })
+
+      parent.appendChild(svg.node());
+      parent.appendChild(tooltip.node());
+      parent.groups = groups;
+
+    } else {
+
+
+      const civs = d35.selectAll(".civ")
+
+      civs.data(filteredData, d => d.actor)
+        .transition()
+        // .delay((d,i)=>i*10)
+        .ease(d35.easeCubic)
+        .attr("transform", (d, i) => `translate(0 ${y(i)})`)
+
+    }
+
+  });
+}
